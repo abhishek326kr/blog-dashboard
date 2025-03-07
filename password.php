8e7ffacfb2029746e01ae98c36f1bcd414718501
Abhishek Kumar <abhishek326kr@gmail.com>
2025-03-07 17:08:27 +0530

    m

--- password.php
+++ password.php
@@ -1,3 +1,28 @@
 <?php
-echo password_hash('editor123', PASSWORD_BCRYPT);
+echo password_hash('swarnalata101', PASSWORD_BCRYPT);
 ?>
+
+
+
+Flexy Markets Bloging Dashboard
+
+URL: https://flexymarkets.com/blog-dashboard
+
+email: payel@flexymarkets.com
+password: payel101
+
+
+email: sayan@flexymarkets.com
+password: sayan101
+
+
+email: rimi@flexymarkets.com
+password: rimi101
+
+
+email: aditi@flexymarkets.com
+password: aditi101
+
+
+email: swarnalata@flexymarkets.com
+password: swarnalata101
